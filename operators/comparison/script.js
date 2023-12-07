91b67fdb01dcc24b2057e17f480391261168732d
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-07 08:20:46 -0800

    add script.js file

---
+++ operators/comparison/script.js
@@ -0,0 +1,15 @@
+let x = 5;
+const y = 3;
+const z = "5";
+
+console.log(x == y);
+console.log(x == z);
+
+let a = 8;
+let b = "8";
+console.log(a === b);
+console.log(a != b);
+console.log(a !==b);
+
+console.log(2 >= 2);
+console.log(3 <= 3 )
