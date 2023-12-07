f8e619525d8234c9891c2997e904c26211a523bb
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-07 07:59:24 -0800

    add script.js file

---
+++ operators/arithmetic/script.js
@@ -0,0 +1,10 @@
+let x = 10;
+const y = 6;
+
+//const result = x + y
+//const result = x - y
+//const result = x * y
+//const result = x / y
+//const result = x ** y
+const result = x % y
+console.log(result);
