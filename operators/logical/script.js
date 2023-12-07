c81d51e24ae9e6f3e0f0262db6dddc1a6bc0dd3d
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-07 08:29:51 -0800

    add script.js file

---
+++ operators/logical/script.js
@@ -0,0 +1,14 @@
+console.log(true && true);
+console.log(true && false);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || false);
+
+const x = 5;
+const y = 3;
+const z = "5";
+
+console.log(x > y && x === z );
+console.log( x > y || x == z)
