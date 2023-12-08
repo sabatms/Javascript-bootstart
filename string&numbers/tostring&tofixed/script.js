4f2ad69f154889667d6ea9512163b7844100dff3
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-08 01:12:20 -0800

    add script.js file

---
+++ string&numbers/tostring&tofixed/script.js
@@ -0,0 +1,15 @@
+const number = 50
+console.log(number);
+console.log(typeof number);
+
+const string = number.toString();
+console.log(string);
+console.log(typeof string);
+
+//tostring = میاد عدد رو تبدیل ب رشته برای ما انجام میده.
+
+const number1 = 4.345123456789;
+const fixed = number1.toFixed(5);
+console.log(fixed);
+
+//Tofixed() = میاد اگر اعداد اعشاری داریم رو داخل اون پرانتز به تعداد اعشاری که میخوایم برای ما جداش میکنه 
