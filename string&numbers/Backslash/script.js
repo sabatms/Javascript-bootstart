9f2b3cce8d61d9dc9c1bb9145e585fd105f18b1f
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-08 02:08:12 -0800

    add script.js

---
+++ string&numbers/Backslash/script.js
@@ -0,0 +1,15 @@
+const text = "Hi i'am saba \
+and i live in tehran";
+console.log(text);
+
+// back slash= میاداگر جمله ای ما طولانی باشه با \ اونا رو از هم جدا میکنه و میره خط بعد در متغیر
+
+const text1 = "hi \n everyone \n;  today is great?"
+console.log(text1);
+
+// (\n) = میاد اگر بخوایم موقعی که خروجی ما در خط بعد بیوفتد از  آن استفاده میکنیم
+
+const text2 = "Text content";
+console.log(text2.length);
+
+// length = میاد طول رو به ما میده
