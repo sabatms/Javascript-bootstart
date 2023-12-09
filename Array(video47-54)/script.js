021210018a74102240fdc7dfab70e8bdfe8dd125
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-09 05:37:19 -0800

    add script.js file

---
+++ Array(video47-54)/script.js
@@ -0,0 +1,62 @@
+// const firstArray = [1,2,3];
+// console.log(firstArray);
+
+// const secondArray = Array(4,5,6);
+// console.log(secondArray);
+
+// Array :
+
+const myArray = ["fatii", "tahmasebishz", 25 , 2, 4, 6, 8];
+// const array2 = [10,20,30];
+// const newArray = [
+//  [1,2,3],
+//  [5,6,7] ,
+// ];
+// console.log(myArray);
+// console.log(myArray[0]);
+//myArray.push(1);
+//myArray.unshift(1);
+//myArray.pop();
+//myArray.shift()
+// const newArray = myArray.toString();
+//console.log(newArray);
+//myArray[0] = "Sanam";
+// const string = myArray.join(" and ");
+// console.log(string);
+// const newArray = myArray.concat(array2);
+// console.log(newArray);
+// const finalArray = newArray.flat();
+// console.log(finalArray);
+// console.log(myArray.indexOf("tahmasebishz"));
+// console.log(myArray.includes("tahmasebishz"));
+// const newArray = myArray.slice(-4 , -2);
+// console.log(newArray);
+// const newArray = myArray.splice(-2);
+// console.log(newArray);
+//myArray.sort();
+myArray.reverse()
+console.log(myArray);
+
+/*
+: داریم index برای اینکه بفهمیم چندتا
+هست یکی کمتر است(lenght)همیشه تعداد آنها از طول 
+*/
+// push = به اخر ارایه اضافه میشه
+// unshift = به اول ارایه اضافه  میکنه
+// pop =  از اخر ارایه حذف میکنه
+// shift =  از اول ارایه کم میکنه
+// delete & tostring();
+// toString() = میاد ارایه ها رو تبدیل به رشته میکنه
+/*
+ برای اینکه بخوایم یک ارایه رو جایگزین کنیم
+ اون ارایه index ابتدا میایم به  
+مقداری که میخوایم عوض کیم تغیرش میدیم 
+ */
+
+// join =  میاد ارایه رو ترکیب میکنه و یک رشته رو به ما میده و حتی شده یک کلمه یک حرف یک اسپیس
+// concat = میاد یک ارایه به یک ارایه دیگه وصل میکنه
+// flat = میاد اگر چندتا ارایه تو در تو داشتیم و اینکه بخوایم تمام ارایه را در یک خط به صورت خوانا قرار بدیم از این ایتم استفاده میکنیم
+// indexOf() =قرار داره index میاد میبینه که ارایه در چه قسمتی از
+// includes =  رو به نمایش میدهboolean میاد
+// sort () = میاد ترتیب رو به صورت منظم میکنه
+// reverse() = میاد ترتیب رو برای ما برعکس میکنه
