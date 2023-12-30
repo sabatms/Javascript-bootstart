318105360a60699d9fce058d27ef6d19a28a539b
Saba Tahmasebi <saba.tms15@gmail.com>
2023-12-30 03:48:16 -0800

    test saba & eli

---
+++ conditional statements & Loops/script.js
@@ -0,0 +1,56 @@
+// const number = 5;
+// if( number > 0){
+// console.log("This number is posetive");
+// }else if (number < 0){
+// console.log("This number is negetive");
+// } else{
+// console.log("this number is 0")
+// }
+
+// const number = 6;
+// if(typeof number === "number" && number > 0 ){
+// console.log("This number is positive");
+// }
+
+// const side1 = 4;
+// const side2 = 4;
+// const side3 = 4;
+// if ( side1 === side2 && side2 === side3){
+// console.log("motesavi azla");
+// } else if( side1 === side2 || side1 === side3 || side2 === side3){
+// console.log("motesavi alsaghein");
+// }else{
+// console.log("makhrot");
+// }
+
+// const firstName = "Ali";
+// switch(firstName){
+// case "milad":
+// console.log("you are milad");
+// break
+// case "Ali":
+// console.log("you are Ali");
+// break
+// default:
+// console.log("who are you");
+// }
+
+// for(let i = 1; i < 10; i++){
+// console.log(i)
+// }
+
+// const array = [2, 4, 6, 8, 10];
+// for(let i = 0; i < array.length; i++){
+// console.log(array[i] + 4) ;
+// }
+
+// const array = [1, 3, 5, 7]
+// for(let i of array){
+// console.log(i + 5);
+// }
+
+// const object = {name : "saba", age : 24};
+// for (let key in object){
+// console.log(key)
+// console.log(object[key]);
+// }
